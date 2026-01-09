4475e30d4132e7f4bc689a0c013130cf0a1a0f10
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-01-09 18:09:52 +0000

    add pandas pipeline with from_numpy function

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Documented"""
+import pandas as pd
+
+
+def from_numpy(array):
+    """Documented"""
+    df = pd.DataFrame(array,
+                      columns=[chr(i + 65) for i in range(array.shape[1])])
+    return df
