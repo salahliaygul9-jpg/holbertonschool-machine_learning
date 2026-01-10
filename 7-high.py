225b3d8434bc478ff1feb260c29e679f63bfcfbd
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-01-10 23:08:46 +0000

    sort

---
+++ 7-high.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+This module fills missing data in a DataFrame
+'''
+
+def high(df):
+    '''
+    Fill missing values in the DataFrame
+    '''
+    df['Close'] = df['Close'].ffill()  # method='pad' yerinə ffill
+    df['High'] = df['High'].fillna(df['Close'])
+    df['Low'] = df['Low'].fillna(df['Close'])
+    df['Open'] = df['Open'].fillna(df['Close'])
+    df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+    df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
+    return df
