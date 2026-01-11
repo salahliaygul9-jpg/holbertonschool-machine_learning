6ab171c4be8644de733749773e1028993693c924
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-01-11 01:15:22 +0000

    Line Graph

---
+++ 0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+    
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    x = np.arange(0, 10)
+    plt.plot(range(11), y, linestyle='solid', color='red')
+    plt.xlim(0, 10)
+    plt.show()
