bb909f44d6aac669019d55770ffb0bc596c51e3f
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-01-11 00:27:01 +0000

    Indexing

---
+++ pipeline/pandas/10-index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function def index(df):
+"""
+
+
+def index(df):
+    """
+    function def index(df): that takes a pd.DataFrame
+    """
+    return df.set_index("Timestamp")
