d06696483f9e5fe16ad166f59359379eb6d4d3c5
Salahliaygul9-jpg <Salahliaygul9@gmail.com>
2026-01-11 13:19:36 +0400

    Change of scale

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module for Exponential Decay
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """Plot C-14 exponential decay as a line plot with log-scaled y-axis."""
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.plot(x, y)
+    plt.yscale('log')
+    plt.title('Exponential Decay of C-14')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.xlim(0, 28650)
+    plt.show()
