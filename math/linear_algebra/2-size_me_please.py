b7f00df89087347f090c05b1265ea44a77289856
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-01-28 19:26:30 +0000

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-""" Recursion function to check the shape of the matrix"""
+'''Size me up function'''
 
 
 def matrix_shape(matrix):
-    """ return the shape of a matrix """
-    if type(matrix[0]) != list:
-        return [len(matrix)]
-    else:
-        return [len(matrix)] + matrix_shape(matrix[0])
+    '''Comment for matrix shape function'''
+    result = []
+    while (type(matrix) is list):
+        result.append(len(matrix))
+        matrix = matrix[0]
+    return result
+
+
+mat1 = [[1, 2], [3, 4]]
+matrix_shape(mat1)
+mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+matrix_shape(mat2)
