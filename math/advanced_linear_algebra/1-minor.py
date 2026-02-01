9e63903dbd260c4f8b78f44c83f157e6a0cfbce4
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-02-01 19:48:46 +0000

    Minor

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,68 +1,144 @@
 #!/usr/bin/env python3
-"""1. Minor"""
+"""Module to calculate the minor matrix of a matrix
+"""
+
+
+def fourXfour(M):
+    """
+       Computes determinant of 4x4 matrix.
+
+       Args:
+         M: list of lists whose determinant should
+           be calculated.
+
+       Return:
+         Determinant of the matrix.
+    """
+    det = (M[0][0]*M[1][1])-(M[0][1]*M[1][0])
+    return det
+
+
+def M_ij(matrix, i, j):
+    """
+       Returns the matrix minus row [i] and column [j].
+
+       Args:
+         matrix: list of lists to adjust.
+         i: row to remove.
+         j: column to remove.
+
+       Return:
+         The input matrix minus row [i] and column [j].
+    """
+    matrix.pop(i)
+    for row in matrix:
+        row.pop(j)
+
+    return matrix
+
+
+def deep_cp(mat):
+    """Copies list of lists"""
+    return [x.copy() for x in mat]
+
+
+def recurse_determinant(matrix):
+    """
+       Finds the determinant of a NxN matrix.
+
+       Args:
+         matrix: list of lists whose determinant should
+           be calculated.
+
+       Return:
+         Determinant of the matrix.
+    """
+    if len(matrix) == 2:
+        return fourXfour(matrix)
+
+    det, flip = 0, 1
+
+    for x, num in enumerate(matrix[0]):
+        sub_matrix = M_ij(deep_cp(matrix), 0, x)
+        det += flip*num*recurse_determinant(sub_matrix)
+        flip = flip * -1
+
+    return det
 
 
 def determinant(matrix):
-    """A function that calculates the determinant of a matrix"""
+    """
+       Finds the determinant of a NxN matrix.
+
+       Args:
+         matrix: list of lists whose determinant should
+           be calculated using recurse_determinant().
+
+       Return:
+         Determinant of the matrix.
+    """
     if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
 
-    if matrix == [[]]:
-        return 1
+    H = len(matrix)
 
-    for i in range(len(matrix)):
-        if len(matrix) != len(matrix[i]):
-            raise ValueError("matrix must be a square matrix")
-        if type(matrix[i]) is not list or not len(matrix[i]):
+    if H == 0:
+        raise TypeError("matrix must be a list of lists")
+
+    for item in matrix:
+        if type(item) is not list:
             raise TypeError("matrix must be a list of lists")
+        if len(item) != H and H != 1:
+            raise ValueError("matrix must be a non-empty square matrix")
 
-    if len(matrix) == 1 and len(matrix[0]) == 1:
-        return matrix[0][0]
-
-    if len(matrix) == 2 and len(matrix[0]) == 2:
-        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
-
-    first_row = matrix[0]
-    determ = 0
-    cof = 1
-    for i in range(len(matrix[0])):
-        next_matrix = [x[:] for x in matrix]
-        del next_matrix[0]
-        for mat in next_matrix:
-            del mat[i]
-        determ += first_row[i] * determinant(next_matrix) * cof
-        cof = cof * -1
-
-    return determ
+    if H == 1:
+        if len(matrix[0]) == 0:
+            raise 1
+        elif len(matrix[0]) == 1:
+            return matrix[0][0]
+        else:
+            raise ValueError("matrix must be a non-empty square matrix")
+    else:
+        return recurse_determinant(matrix)
 
 
 def minor(matrix):
-    """A function that calculates the minor matrix of a matrix"""
-    if type(matrix) is not list or not len(matrix):
+    """
+       Calculates minor matrix of a matrix.
+
+       Args:
+         matrix: the matrix to find the minors of.
+
+       Return:
+         the matrix of minors.
+    """
+
+    if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
 
-    if matrix == [[]]:
-        raise ValueError("matrix must be a non-empty square matrix")
-    
-    for i in range(len(matrix)):
-        if len(matrix) != len(matrix[i]):
-            raise ValueError("matrix must be a non-empty square matrix")
-        if type(matrix[i]) is not list or not len(matrix[i]):
+    H = len(matrix)
+
+    if H == 0:
+        raise TypeError("matrix must be a list of lists")
+
+    for item in matrix:
+        if type(item) is not list:
             raise TypeError("matrix must be a list of lists")
-    
-    if len(matrix) == 1:
+        if len(item) != H:
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if H == 1:
         return [[1]]
-    
-    minor = []
+
+    m, minors = [], []
+
     for i in range(len(matrix)):
-        inner = []
-        for j in range(len(matrix)):
-            mat = [l[:] for l in matrix]
-            del mat[i]
-            for m in mat:
-                del m[j]
-            det = determinant(mat)
-            inner.append(det)
-        minor.append(inner)
-    
-    return minor
+        for j in range(len(matrix[0])):
+            m.append(
+                determinant(M_ij(deep_cp(matrix), i, j))
+            )
+
+        minors.append(m.copy())
+        m.clear()
+
+    return minors
