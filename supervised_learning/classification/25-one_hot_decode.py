5803fd1f75b8fee5dbfd8821def96c8114d9a6f7
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-03-13 19:48:21 +0000

    One-Hot Decode

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""One hot decoding."""
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """One hot deconding on encoded array."""
+    if type(one_hot) is not np.ndarray:
+        return None
+    if len(one_hot.shape) != 2:
+        return None
+    return np.where(one_hot.T == 1)[1]
