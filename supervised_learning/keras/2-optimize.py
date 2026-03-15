28a763d39f56e960fdbf939b08dcea7d45a49edd
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-03-15 17:26:21 +0000

    Optimize

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""A function that sets up Adam optimization for a keras model
-with categorical crossentropy loss and accuracy metrics"""
+"""Script to implement ADAM optimization using keras"""
+
 import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """A function that sets up Adam optimization for a keras
-    model with categorical croddentropy loss and accuracy
-    metrics"""
-    network.compile(optimizer=K.optimizers.Adam(lr=alpha,
-                                                beta_1=beta1,
-                                                beta_2=beta2),
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'])
-
+    """sets up the optimizer and compiles the model"""
+    adam = K.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2
+    )
+    network.compile(
+        optimizer=adam,
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
     return None
