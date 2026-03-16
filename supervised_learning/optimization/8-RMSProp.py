ccf1fe9bd3261c42b5b5792f8cdce3470b3be1aa
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-03-16 13:20:35 +0000

    RMSProp Upgraded

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python3
-"""
-RMSProp ugraded
-"""
-
-
+"""Implementing optimizations."""
 import tensorflow as tf
 
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """
-    Function that creates the training operation for a NN in tensorflow
-    using the RMSProp optimization algorithm
-    Arguments:
-     - loss is the loss of the network
-     - alpha is the learning rate
-     - beta2 is the RMSProp weight
-     - epsilon is a small number to avoid division by zero
-    Returns:
-     The RMSProp optimization operation
-    """
-
-    optimizer = tf.train.RMSPropOptimizer(alpha, epsilon=epsilon, decay=beta2)
-    train = optimizer.minimize(loss)
-
-    return train
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """Set up the RMSProp optimization algorithm in TensorFlow."""
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon
+    )
+    return optimizer
