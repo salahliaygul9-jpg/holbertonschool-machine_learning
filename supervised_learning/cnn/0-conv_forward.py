a53afdfce728554dd7efef0e4417f9fdb5e2646d
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-04-05 18:02:05 +0000

    Convolutional Forward Prop

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,34 +1,63 @@
 #!/usr/bin/env python3
-""" convolutions"""
+"""
+Convolution forward
+"""
+
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    """ conv forward"""
-    hk = W.shape[0]
-    wk = W.shape[1]
-    nc = W.shape[3]
-    m = A_prev.shape[0]
-    hm = A_prev.shape[1]
-    wm = A_prev.shape[2]
-    st1 = stride[1]
-    st0 = stride[0]
-    if padding == "valid":
-        pad0 = 0
-        pad1 = 0
-    else:
-        pad0 = int(((hm - 1) * st0 + hk - hm) / 2)
-        pad1 = int(((wm - 1) * st1 + wk - wm) / 2)
-    out_h = int((hm + 2 * pad0 - hk) / st0) + 1
-    out_w = int((wm + 2 * pad1 - wk) / st1) + 1
-    convoluted = np.zeros((m, out_h, out_w, nc))
-    img = np.pad(A_prev, ((0, 0), (pad0, pad0), (
-                           pad1, pad1), (0, 0)), 'constant')
-    for c in range(nc):
-        for h in range(out_h):
-            for w in range(out_w):
-                ma = img[:, h * st0: h * st0 + hk, w * st1: w * st1 + wk]
-                vm = np.sum(ma * W[..., c], axis=(1, 2, 3))
-                convoluted[:, h, w, c] = vm
-    z = convoluted + b
-    return(activation(z))
+    """Performs forward propagation over a convolutional layer of a neural
+    network.
+
+    Args:
+        A_prev (np.ndarray): matrix of shape (m, h_prev, w_prev, c_prev)
+                             containing the output of the previous layer.
+        W (np.ndarray): matrix of shape (kh, kw, c_prev, c_new) containing
+                        the kernels for the convolution.
+        b (np.ndarray): matrix of shape (1, 1, 1, c_new) containing the biases
+                        applied to the convolution.
+        activation (function): activation function applied to the convolution.
+        padding (str): same or valid, indicating the type of padding used.
+        stride (tuple): shape (sh, sw) containing the strides for the
+                        convolution.
+
+    Returns:
+
+    """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, c_new = W.shape
+    sh, sw = stride
+
+    ph = 0
+    pw = 0
+
+    if padding == 'same':
+        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
+        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+
+    if type(padding) == tuple:
+        ph, pw = padding
+
+    padded_img = np.pad(A_prev,
+                        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                        'constant')
+
+    ch = int(((h_prev + 2 * ph - kh) / sh) + 1)
+    cw = int(((w_prev + 2 * pw - kw) / sw) + 1)
+
+    Aprev_conv_W = np.zeros((m, ch, cw, c_new))
+
+    for j in range(ch):
+        for k in range(cw):
+            for n in range(c_new):
+                images_slide = padded_img[:,
+                                          j * sh:j * sh + kh,
+                                          k * sw:k * sw + kw]
+                kernel = W[:, :, :, n]
+                elem_mul = np.multiply(images_slide, kernel)
+                Aprev_conv_W[:, j, k, n] = elem_mul.sum(axis=(1, 2, 3))
+
+    Z = Aprev_conv_W + b
+
+    return activation(Z)
