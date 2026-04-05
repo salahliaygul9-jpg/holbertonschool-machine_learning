459d23dedf67d712d96908177efab0b945b2dfb4
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-04-05 18:07:32 +0000

    Convolutional Forward Prop

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,63 +1,37 @@
 #!/usr/bin/env python3
-"""
-Convolution forward
-"""
-
+"""Convolutional Forward Prop"""
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    """Performs forward propagation over a convolutional layer of a neural
-    network.
-
-    Args:
-        A_prev (np.ndarray): matrix of shape (m, h_prev, w_prev, c_prev)
-                             containing the output of the previous layer.
-        W (np.ndarray): matrix of shape (kh, kw, c_prev, c_new) containing
-                        the kernels for the convolution.
-        b (np.ndarray): matrix of shape (1, 1, 1, c_new) containing the biases
-                        applied to the convolution.
-        activation (function): activation function applied to the convolution.
-        padding (str): same or valid, indicating the type of padding used.
-        stride (tuple): shape (sh, sw) containing the strides for the
-                        convolution.
-
-    Returns:
-
-    """
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, _, c_new = W.shape
+    """Return the output of the convolutional laye"""
+    (m, h_prev, w_prev, c_prev) = A_prev.shape
+    (kh, kw, c_prev, c_new) = W.shape
     sh, sw = stride
-
-    ph = 0
-    pw = 0
-
     if padding == 'same':
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+        ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
+        pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
 
-    if type(padding) == tuple:
-        ph, pw = padding
+    if padding == 'valid':
+        ph = 0
+        pw = 0
 
-    padded_img = np.pad(A_prev,
-                        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        'constant')
+    new_w = (w_prev + 2 * pw - kw) // sw + 1
+    new_h = (h_prev + 2 * ph - kh) // sh + 1
+    convolved = np.zeros((m, new_h, new_w, c_new))
 
-    ch = int(((h_prev + 2 * ph - kh) / sh) + 1)
-    cw = int(((w_prev + 2 * pw - kw) / sw) + 1)
+    image_padded = np.pad(
+        A_prev,
+        pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode="constant",
+        constant_values=0)
 
-    Aprev_conv_W = np.zeros((m, ch, cw, c_new))
-
-    for j in range(ch):
-        for k in range(cw):
-            for n in range(c_new):
-                images_slide = padded_img[:,
-                                          j * sh:j * sh + kh,
-                                          k * sw:k * sw + kw]
-                kernel = W[:, :, :, n]
-                elem_mul = np.multiply(images_slide, kernel)
-                Aprev_conv_W[:, j, k, n] = elem_mul.sum(axis=(1, 2, 3))
-
-    Z = Aprev_conv_W + b
-
+    for k in range(c_new):
+        for i in range(new_h):
+            for j in range(new_w):
+                convolved[:, i, j, k] = np.sum(
+                    image_padded[:, i * sh:i * sh + kh,
+                                 j * sw:j * sw + kw, :] * W[:, :, :, k],
+                    axis=(1, 2, 3))
+    Z = convolved + b
     return activation(Z)
