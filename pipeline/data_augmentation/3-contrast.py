19286ef438564d6e49cdab851414fbe331e0f661
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-04-12 18:41:29 +0000

    Contrast

--- pipeline/data_augmentation/3-contrast.py
+++ pipeline/data_augmentation/3-contrast.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-"""Contrast"""
+"""
+Contrast
+"""
 import tensorflow as tf
 
 
-def shear_image(image, intensity):
-    """Shears an image based on intensity"""
-    image = tf.expand_dims(image, axis=0)
-    datagen = (tf.keras.preprocessing.image.
-               ImageDataGenerator(shear_range=intensity))
-    datagen.fit(x=image)
-    new_image = datagen.flow(image)
-    new_image = new_image.next()[0].astype("int")
-    return new_image
+def change_contrast(image, lower, upper):
+    """
+    """
+    return tf.image.random_contrast(image, lower, upper)
