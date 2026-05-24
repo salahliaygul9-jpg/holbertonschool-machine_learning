370fee45a110f796bc11e7fa506b1a8de3d436ea
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-05-24 18:26:50 +0000

    BIC

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
-"""Bayesian Information Criterion for GMM module"""
+"""Bayesian Information Criterion for GMM"""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """Finds the best number of clusters for a GMM using BIC."""
+    """Finds the best number of clusters for a GMM using BIC.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        kmin: positive integer, minimum number of clusters (inclusive)
+        kmax: positive integer, maximum number of clusters (inclusive)
+        iterations: positive integer, maximum iterations for EM
+        tol: non-negative float, tolerance for EM
+        verbose: boolean, whether EM should print info
+
+    Returns:
+        best_k, best_result, l, b or None, None, None, None on failure
+    """
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None
     if not isinstance(kmin, int) or kmin <= 0:
@@ -16,41 +28,30 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
     if not isinstance(verbose, bool):
         return None, None, None, None
-
     n, d = X.shape
-
     if kmax is None:
         kmax = n
     if not isinstance(kmax, int) or kmax <= 0:
         return None, None, None, None
-    if kmax < kmin or kmax > n:
+    if kmax < kmin + 1:
         return None, None, None, None
-    if kmax - kmin + 1 < 2:
-        return None, None, None, None
-
-    likelihoods = []
-    bics = []
+    l_list = []
+    b_list = []
     results = []
-
     for k in range(kmin, kmax + 1):
-        pi, m, S, g, lk = expectation_maximization(
+        pi, m, S, g, ll = expectation_maximization(
             X, k, iterations, tol, verbose)
         if pi is None:
             return None, None, None, None
-
         results.append((pi, m, S))
-        likelihoods.append(lk)
-
-        # p = number of parameters:
-        # k-1 priors + k*d means + k*d*(d+1)/2 covariance entries
-        p = k - 1 + k * d + k * d * (d + 1) // 2
-        bics.append(p * np.log(n) - 2 * lk)
-
-    l = np.array(likelihoods)
-    b = np.array(bics)
-
+        l_list.append(ll)
+        p = k * d * (d + 1) / 2 + d * k + (k - 1)
+        bic = p * np.log(n) - 2 * ll
+        b_list.append(bic)
+    ll = np.array(l_list)
+    b = np.array(b_list)
     best_idx = np.argmin(b)
     best_k = kmin + best_idx
     best_result = results[best_idx]
 
-    return best_k, best_result, l, b
+    return best_k, best_result, ll, b
