dea85058ee3da7b70df7ce8dcdc1e4534c84e6c7
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-05-24 17:55:21 +0000

    EM

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Mon Mar 25 08:33:12 2021
+Expectation maximization algorithm with GMM
+"""
 
-@author: Robinson Montes
-"""
 import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
@@ -13,62 +11,63 @@ maximization = __import__('7-maximization').maximization
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
-    Function that performs the expectation maximization for a GMM:
+    Performs the expectation-maximization (EM) algorithm for a Gaussian Mixture
+    Model (GMM) on a given dataset.
 
-    Arguments:
-     - X is a numpy.ndarray of shape (n, d) containing the data set
-     - k is a positive integer containing the number of clusters
-     - iterations is a positive integer containing the maximum number
-        of iterations for the algorithm
-     - tol is a non-negative float containing tolerance of the log likelihood,
-        used to determine early stopping i.e. if the difference is less than
-        or equal to tol you should stop the algorithm
-     - verbose is a boolean that determines if you should print information
-        about the algorithm
+    Parameters:
+    - X (numpy.ndarray): dataset to be clustered, of shape (n, d) where n is
+    the number of data points and d is the dimensionality of each data point.
+    - k (int): The number of clusters.
+    - iterations (int, optional): The maximum number of iterations for the
+    algorithm (default is 1000).
+    - tol (float, optional): The tolerance of the log likelihood for early
+    stopping (default is 1e-5).
+    - verbose (bool, optional): If True, prints the log likelihood after every
+    10 iterations and after the last iteration (default is False).
 
     Returns:
-     pi, m, S, g, l, or None, None, None, None, None on failure
-     - pi is a numpy.ndarray of shape (k,) containing the priors for
-        each cluster
-     - m is a numpy.ndarray of shape (k, d) containing the centroid means for
-        each cluster
-     - S is a numpy.ndarray of shape (k, d, d) containing the covariance
-        matrices for each cluster
-     - g is a numpy.ndarray of shape (k, n) containing the probabilities for
-        each data point in each cluster
-     - l is the log likelihood of the model
+    - pi (numpy.ndarray): The priors for each cluster, of shape (k,).
+    - m (numpy.ndarray): The centroid means for each cluster, of shape (k, d).
+    - S (numpy.ndarray): The covariance matrices for each cluster, of
+    shape (k, d, d).
+    - g (numpy.ndarray): The posterior probabilities for each data point in
+    each cluster, of shape (k, n).
+    - l (float): The log likelihood of the model.
+
+    If the function fails, it returns (None, None, None, None, None).
     """
-
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None, None, None, None
-    if type(k) != int or k <= 0 or k >= X.shape[0]:
-        return None, None, None, None, None
-    if type(iterations) != int or iterations <= 0:
-        return None, None, None, None, None
-    if type(tol) != float or tol <= 0:
-        return None, None, None, None, None
-    if type(verbose) != bool:
+    if (
+        not isinstance(X, np.ndarray) or X.ndim != 2
+        or not isinstance(k, int) or k <= 0
+        or not isinstance(iterations, int) or iterations <= 0
+        or not isinstance(tol, float) or tol < 0
+        or not isinstance(verbose, bool)
+    ):
         return None, None, None, None, None
 
+    # Initialize priors, centroid means, and covariance matrices
     pi, m, S = initialize(X, k)
-    prev_like = 0
-    g, likelihood = expectation(X, pi, m, S)
 
     for i in range(iterations):
-        if verbose and (i % 10 == 0):
-            msg = 'Log Likelihood after {} iterations: {}'\
-                .format(i, likelihood.round(5))
-            print(msg)
+        # Evaluate the probabilities and likelihoods with current parameters
+        g, prev_li = expectation(X, pi, m, S)
+
+        # In verbose mode, print the likelihood every 10 iterations after 0
+        if verbose and i % 10 == 0:
+            print(f"Log Likelihood after {i} iterations: {round(prev_li, 5)}")
+
+        # Re-estimate the parameters with the new values
         pi, m, S = maximization(X, g)
-        g, likelihood = expectation(X, pi, m, S)
 
-        if abs(likelihood - prev_like) <= tol:
+        # Evaluate new log likelihood
+        g, li = expectation(X, pi, m, S)
+
+        # If the likelihood varied by less than the tolerance value, we stop
+        if np.abs(li - prev_li) <= tol:
             break
-        prev_like = likelihood
 
+    # Last verbose message with current likelihood
     if verbose:
-        msg = 'Log Likelihood after {} iterations: {}'\
-            .format(i + 1, likelihood.round(5))
-        print(msg)
-
-    return pi, m, S, g, likelihood
+        # NOTE i + 1 since it has been updated once more since last print
+        print(f"Log Likelihood after {i + 1} iterations: {round(li, 5)}")
+    return pi, m, S, g, li
