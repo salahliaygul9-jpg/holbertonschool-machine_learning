db28922f168c1ef97f373a80b042d52bf73cde89
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-05-24 17:52:16 +0000

    EM

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,74 +1,74 @@
 #!/usr/bin/env python3
-"""Expectation Maximization for GMM"""
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 08:33:12 2021
+
+@author: Robinson Montes
+"""
 import numpy as np
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
-    Performs the expectation maximization for a GMM
+    Function that performs the expectation maximization for a GMM:
 
-    Parameters:
-        X: numpy.ndarray of shape (n, d) containing the data set
-        k: positive integer containing the number of clusters
-        iterations: positive integer containing the maximum number of iterations
-        tol: non-negative float containing tolerance of the log likelihood
-        verbose: boolean that determines if you should print information
+    Arguments:
+     - X is a numpy.ndarray of shape (n, d) containing the data set
+     - k is a positive integer containing the number of clusters
+     - iterations is a positive integer containing the maximum number
+        of iterations for the algorithm
+     - tol is a non-negative float containing tolerance of the log likelihood,
+        used to determine early stopping i.e. if the difference is less than
+        or equal to tol you should stop the algorithm
+     - verbose is a boolean that determines if you should print information
+        about the algorithm
 
     Returns:
-        pi, m, S, g, l or None, None, None, None, None on failure
+     pi, m, S, g, l, or None, None, None, None, None on failure
+     - pi is a numpy.ndarray of shape (k,) containing the priors for
+        each cluster
+     - m is a numpy.ndarray of shape (k, d) containing the centroid means for
+        each cluster
+     - S is a numpy.ndarray of shape (k, d, d) containing the covariance
+        matrices for each cluster
+     - g is a numpy.ndarray of shape (k, n) containing the probabilities for
+        each data point in each cluster
+     - l is the log likelihood of the model
     """
+
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-    if not isinstance(k, int) or k <= 0:
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
         return None, None, None, None, None
-    if not isinstance(iterations, int) or iterations <= 0:
+    if type(iterations) != int or iterations <= 0:
         return None, None, None, None, None
-    if not isinstance(tol, float) or tol < 0:
+    if type(tol) != float or tol <= 0:
         return None, None, None, None, None
-    if not isinstance(verbose, bool):
+    if type(verbose) != bool:
         return None, None, None, None, None
-
-    initialize = __import__('4-initialize').initialize
-    expectation = __import__('6-expectation').expectation
-    maximization = __import__('7-maximization').maximization
 
     pi, m, S = initialize(X, k)
-    if pi is None or m is None or S is None:
-        return None, None, None, None, None
-
-    l_prev = 0
-    g = None
-    l = None
+    prev_like = 0
+    g, likelihood = expectation(X, pi, m, S)
 
     for i in range(iterations):
-        # E-step
-        g, l = expectation(X, pi, m, S)
-        if g is None or l is None:
-            return None, None, None, None, None
-
-        # Print verbose output every 10 iterations
-        if verbose and i % 10 == 0:
-            print("Log Likelihood after {} iterations: {}".format(
-                i, round(l, 5)))
-
-        # Check for convergence
-        if i > 0 and abs(l - l_prev) <= tol:
-            break
-
-        l_prev = l
-
-        # M-step
+        if verbose and (i % 10 == 0):
+            msg = 'Log Likelihood after {} iterations: {}'\
+                .format(i, likelihood.round(5))
+            print(msg)
         pi, m, S = maximization(X, g)
-        if pi is None or m is None or S is None:
-            return None, None, None, None, None
+        g, likelihood = expectation(X, pi, m, S)
 
-    # Final E-step after last maximization
-    g, l = expectation(X, pi, m, S)
-    if g is None or l is None:
-        return None, None, None, None, None
+        if abs(likelihood - prev_like) <= tol:
+            break
+        prev_like = likelihood
 
     if verbose:
-        print("Log Likelihood after {} iterations: {}".format(
-            i + 1, round(l, 5)))
+        msg = 'Log Likelihood after {} iterations: {}'\
+            .format(i + 1, likelihood.round(5))
+        print(msg)
 
-    return pi, m, S, g, l
+    return pi, m, S, g, likelihood
