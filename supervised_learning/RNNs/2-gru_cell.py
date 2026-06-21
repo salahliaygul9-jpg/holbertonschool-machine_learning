7f7f91205c441687f3dda3af169ddddc66d52cdd
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-06-21 14:22:24 +0000

    GRU Cell

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -1,45 +1,53 @@
+#!/usr/bin/env python3
+"""Module that defines a GRU cell."""
 import numpy as np
 
 
 class GRUCell:
-    """Represents a gated recurrent unit cell."""
+    """Represents a gated recurrent unit."""
 
     def __init__(self, i, h, o):
-        self.i = i
-        self.h = h
-        self.o = o
+        """Initialize the GRU cell.
 
+        Args:
+            i (int): Dimensionality of the data.
+            h (int): Dimensionality of the hidden state.
+            o (int): Dimensionality of the output.
+        """
         self.Wz = np.random.randn(i + h, h)
         self.Wr = np.random.randn(i + h, h)
         self.Wh = np.random.randn(i + h, h)
         self.Wy = np.random.randn(h, o)
-
         self.bz = np.zeros((1, h))
         self.br = np.zeros((1, h))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
-    def softmax(self, x):
-        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
-        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
-
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
     def forward(self, h_prev, x_t):
-        concat = np.concatenate((h_prev, x_t), axis=1)
+        """Perform forward propagation for one time step.
 
-        z = self.sigmoid(concat @ self.Wz + self.bz)
-        r = self.sigmoid(concat @ self.Wr + self.br)
+        Args:
+            h_prev (numpy.ndarray): Previous hidden state of shape (m, h).
+            x_t (numpy.ndarray): Input data of shape (m, i).
 
-        r_h = r * h_prev
-        concat_r = np.concatenate((r_h, x_t), axis=1)
+        Returns:
+            tuple: h_next, y
+                h_next is the next hidden state
+                y is the output of the cell
+        """
+        hx = np.concatenate((h_prev, x_t), axis=1)
 
-        h_tilde = np.tanh(concat_r @ self.Wh + self.bh)
+        z = 1 / (1 + np.exp(-(np.matmul(hx, self.Wz) + self.bz)))
+        r = 1 / (1 + np.exp(-(np.matmul(hx, self.Wr) + self.br)))
 
-        h_next = (1 - z) * h_prev + z * h_tilde
+        rh = r * h_prev
+        rhx = np.concatenate((rh, x_t), axis=1)
+        h_hat = np.tanh(np.matmul(rhx, self.Wh) + self.bh)
+        h_next = (1 - z) * h_prev + z * h_hat
 
-        y_linear = h_next @ self.Wy + self.by
-        y = self.softmax(y_linear)
+        y_linear = np.matmul(h_next, self.Wy) + self.by
+        y_shifted = y_linear - np.max(y_linear, axis=1, keepdims=True)
+        y_exp = np.exp(y_shifted)
+        y = y_exp / np.sum(y_exp, axis=1, keepdims=True)
 
         return h_next, y
