8e63b3c5817f8493bf8aae9cbf5024612ab89365
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-06-21 14:11:57 +0000

    RNN

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -1,32 +1,30 @@
+#!/usr/bin/env python3
+"""Module that performs forward propagation for a simple RNN."""
 import numpy as np
 
-def rnn(rnn_cell, X, h_0):
-    """
-    Performs forward propagation for a simple RNN
 
-    X: (t, m, i)
-    h_0: (m, h)
+def rnn(rnn_cell, X, h_0):
+    """Perform forward propagation for a simple RNN.
+
+    Args:
+        rnn_cell: Instance of RNNCell.
+        X (numpy.ndarray): Input data of shape (t, m, i).
+        h_0 (numpy.ndarray): Initial hidden state of shape (m, h).
 
     Returns:
-    H: (t, m, h)
-    Y: (t, m, o)
+        tuple: H, Y
+            H contains all hidden states
+            Y contains all outputs
     """
-
-    t, m, _ = X.shape
-    h = h_0.shape[1]
+    t = X.shape[0]
+    m, h = h_0.shape
     o = rnn_cell.by.shape[1]
 
-    H = np.zeros((t, m, h))
+    H = np.zeros((t + 1, m, h))
     Y = np.zeros((t, m, o))
-
-    h_prev = h_0
+    H[0] = h_0
 
     for step in range(t):
-        x_t = X[step]
-
-        h_prev, y_t = rnn_cell.forward(h_prev, x_t)
-
-        H[step] = h_prev
-        Y[step] = y_t
+        H[step + 1], Y[step] = rnn_cell.forward(H[step], X[step])
 
     return H, Y
