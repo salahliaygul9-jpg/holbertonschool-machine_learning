78044bb4953dfc9158afef1a56938755abb83efb
salahliaygul9-jpg <salahliagul9@gmail.com>
2026-07-03 09:47:21 +0000

    Train Word2Vec

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Word2Vec model module"""
+from gensim.models import Word2Vec
+
+
+def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
+                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
+    """
+    Creates, builds and trains a gensim word2vec model
+
+    Args:
+        sentences: list of sentences to be trained on
+        vector_size: dimensionality of the embedding layer
+        min_count: minimum number of occurrences of a word for use in
+                    training
+        window: maximum distance between the current and predicted word
+                within a sentence
+        negative: size of negative sampling
+        cbow: boolean to determine the training type; True is for CBOW;
+              False is for Skip-gram
+        epochs: number of iterations to train over
+        seed: seed for the random number generator
+        workers: number of worker threads to train the model
+
+    Returns:
+        the trained model
+    """
+    sg = 0 if cbow else 1
+
+    model = Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        seed=seed,
+        workers=workers,
+        epochs=epochs
+    )
+
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=model.epochs
+    )
+
+    return model
