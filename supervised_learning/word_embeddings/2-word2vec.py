8983a756e0c0e3a0e9e2e07ea25dc3eb32c28472
salahliaygul9-jpg <salahliagul9@gmail.com>
2026-07-03 10:50:21 +0000

    Train Word2Vec

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Word2Vec model module"""
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -27,7 +27,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     """
     sg = 0 if cbow else 1
 
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
