e785c0b646edda5a2a350b4c8cc324efbb18699f
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-07-11 07:10:56 +0000

    RNN Decoder

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,79 +1,36 @@
 #!/usr/bin/env python3
-"""RNN Decoder module"""
+"""RNN Decoder Task"""
+
 
 import tensorflow as tf
-
 SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
-    """Decoder for machine translation."""
+    """RNN decoder class"""
 
     def __init__(self, vocab, embedding, units, batch):
-        """
-        Class constructor.
-
-        Args:
-            vocab: size of the output vocabulary
-            embedding: embedding dimension
-            units: number of GRU hidden units
-            batch: batch size
-        """
-        super(RNNDecoder, self).__init__()
-
-        self.embedding = tf.keras.layers.Embedding(
-            input_dim=vocab,
-            output_dim=embedding
-        )
-
-        self.gru = tf.keras.layers.GRU(
-            units,
-            return_sequences=True,
-            return_state=True,
-            recurrent_initializer="glorot_uniform"
-        )
-
+        """RNN Decoder initializer"""
+        super().__init__()
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(units,
+                                       recurrent_initializer="glorot_uniform",
+                                       return_sequences=True,
+                                       return_state=True)
         self.F = tf.keras.layers.Dense(vocab)
 
-        self.attention = SelfAttention(units)
-
     def call(self, x, s_prev, hidden_states):
-        """
-        Forward pass.
+        """Returns the y and s tensors"""
+        items = s_prev.get_shape().as_list()[1]
+        tmp_func = SelfAttention(items)
+        context, weight = tmp_func(s_prev, hidden_states)
 
-        Args:
-            x: previous target word, shape (batch, 1)
-            s_prev: previous decoder hidden state,
-                    shape (batch, units)
-            hidden_states: encoder outputs,
-                           shape (batch, input_seq_len, units)
-
-        Returns:
-            y: output logits, shape (batch, vocab)
-            s: new decoder hidden state, shape (batch, units)
-        """
-        # Compute attention
-        context, _ = self.attention(s_prev, hidden_states)
-
-        # Embed input token
         x = self.embedding(x)
-
-        # Expand context to concatenate with embedding
-        context = tf.expand_dims(context, axis=1)
-
-        # Concatenate context then embedding
+        context = tf.expand_dims(context, 1)
         x = tf.concat([context, x], axis=-1)
+        y, s = self.gru(x)
 
-        # Pass through GRU
-        output, s = self.gru(
-            x,
-            initial_state=s_prev
-        )
-
-        # Remove sequence dimension
-        output = tf.reshape(output, (-1, output.shape[2]))
-
-        # Final dense layer
-        y = self.F(output)
+        y = tf.reshape(y, (-1, y.shape[2]))
+        y = self.F(y)
 
         return y, s
