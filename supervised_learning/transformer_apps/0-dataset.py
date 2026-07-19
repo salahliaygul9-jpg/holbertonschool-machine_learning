5b201e173e7c049dc1ed5daffddb7d948617d039
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-07-19 18:58:40 +0000

    dataset

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python3
-"""Defines the Dataset class for loading and preparing a translation dataset"""
+"""Defines the Dataset class used to load and prep the pt->en
+translation corpus for machine translation.
+"""
 import transformers
 from setup import load_pt2en
 
 
 class Dataset:
-    """Loads and preps a dataset for machine translation"""
+    """Loads and preps a dataset for machine translation."""
 
     def __init__(self):
-        """Initializes the Dataset instance
-
-        Sets:
-            data_train: the ted_hrlr_translate/pt_to_en train split
-            data_valid: the ted_hrlr_translate/pt_to_en validation split
-            tokenizer_pt: the Portuguese tokenizer created from the
-                training set
-            tokenizer_en: the English tokenizer created from the
-                training set
-        """
+        """Sets data_train, data_valid, tokenizer_pt and tokenizer_en."""
         self.data_train = load_pt2en('train')
         self.data_valid = load_pt2en('validation')
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
 
     def tokenize_dataset(self, data):
-        """Creates sub-word tokenizers for our dataset
+        """Creates sub-word tokenizers for the dataset.
 
         Args:
-            data: a tf.data.Dataset whose examples are formatted as a
-                tuple (pt, en)
-                pt: the tf.Tensor containing the Portuguese sentence
-                en: the tf.Tensor containing the corresponding English
-                    sentence
+            data: a Dataset whose examples are formatted as a tuple
+                (pt, en):
+                    pt is the Tensor containing the Portuguese
+                        sentence.
+                    en is the Tensor containing the corresponding
+                        English sentence.
 
         Returns:
-            tokenizer_pt, tokenizer_en: the Portuguese and English
-                tokenizers
+            tokenizer_pt, tokenizer_en: the trained Portuguese and
+                English tokenizers.
         """
-        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+        vocab_size = 2 ** 13
+
+        pt_sentences = []
+        en_sentences = []
+        for pt, en in data.as_numpy_iterator():
+            pt_sentences.append(pt.decode('utf-8'))
+            en_sentences.append(en.decode('utf-8'))
+
+        base_pt = transformers.AutoTokenizer.from_pretrained(
             'neuralmind/bert-base-portuguese-cased')
-        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+        base_en = transformers.AutoTokenizer.from_pretrained(
             'bert-base-uncased')
 
-        def pt_sentences():
-            for pt, en in data.as_numpy_iterator():
-                yield pt.decode('utf-8')
-
-        def en_sentences():
-            for pt, en in data.as_numpy_iterator():
-                yield en.decode('utf-8')
-
-        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
-            pt_sentences(), vocab_size=2 ** 13)
-        tokenizer_en = tokenizer_en.train_new_from_iterator(
-            en_sentences(), vocab_size=2 ** 13)
+        tokenizer_pt = base_pt.train_new_from_iterator(
+            self._batch_iterator(pt_sentences), vocab_size=vocab_size)
+        tokenizer_en = base_en.train_new_from_iterator(
+            self._batch_iterator(en_sentences), vocab_size=vocab_size)
 
         return tokenizer_pt, tokenizer_en
+
+    @staticmethod
+    def _batch_iterator(sentences, batch_size=1000):
+        """Yields successive batches of sentences.
+
+        Args:
+            sentences: list of str sentences.
+            batch_size: number of sentences per yielded batch.
+
+        Yields:
+            list of str, a batch of sentences.
+        """
+        for i in range(0, len(sentences), batch_size):
+            yield sentences[i:i + batch_size]
