6db7256a00209239218f55cb9ac1ab88e2d7961a
salahliaygul9-jpg <salahliaygul9@gmail.com>
2026-07-24 19:34:01 +0000

    dataset

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,61 +1,64 @@
 #!/usr/bin/env python3
 """Dataset class"""
-import tensorflow as tf
 import transformers
 from setup import load_pt2en
 
 
 class Dataset:
-    """Load and prepare the Portuguese-to-English translation dataset."""
+    """Loads and preps a dataset for machine translation."""
 
     def __init__(self):
-        """Initialize the dataset with train/validation splits and
-        tokenizers.
-
-        Creates instance attributes:
-            data_train: Training tf.data.Dataset of (pt, en) string pairs.
-            data_valid: Validation tf.data.Dataset of (pt, en) string pairs.
-            tokenizer_pt: Portuguese sub-word tokenizer.
-            tokenizer_en: English sub-word tokenizer.
-        """
+        """Sets data_train, data_valid, tokenizer_pt and tokenizer_en."""
         self.data_train = load_pt2en('train')
         self.data_valid = load_pt2en('validation')
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train
-        )
+            self.data_train)
 
     def tokenize_dataset(self, data):
-        """Create sub-word tokenizers trained on the dataset.
+        """Creates sub-word tokenizers for the dataset.
 
         Args:
-            data: tf.data.Dataset of (pt, en) tf.string pairs.
+            data: a Dataset whose examples are formatted as a tuple
+                (pt, en):
+                    pt is the Tensor containing the Portuguese
+                        sentence.
+                    en is the Tensor containing the corresponding
+                        English sentence.
 
         Returns:
-            Tuple of (tokenizer_pt, tokenizer_en), both
-            BertTokenizerFast instances with vocab size 2**13.
+            tokenizer_pt, tokenizer_en: the trained Portuguese and
+                English tokenizers.
         """
-        def pt_iterator():
-            """Yield Portuguese sentences from the dataset."""
-            for pt, en in data:
-                yield pt.numpy().decode('utf-8')
+        vocab_size = 2 ** 13
 
-        def en_iterator():
-            """Yield English sentences from the dataset."""
-            for pt, en in data:
-                yield en.numpy().decode('utf-8')
+        pt_sentences = []
+        en_sentences = []
+        for pt, en in data.as_numpy_iterator():
+            pt_sentences.append(pt.decode('utf-8'))
+            en_sentences.append(en.decode('utf-8'))
 
-        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased'
-        )
-        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
-            'bert-base-uncased'
-        )
+        base_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased')
+        base_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased')
 
-        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
-            pt_iterator(), vocab_size=2 ** 13
-        )
-        tokenizer_en = tokenizer_en.train_new_from_iterator(
-            en_iterator(), vocab_size=2 ** 13
-        )
+        tokenizer_pt = base_pt.train_new_from_iterator(
+            self._batch_iterator(pt_sentences), vocab_size=vocab_size)
+        tokenizer_en = base_en.train_new_from_iterator(
+            self._batch_iterator(en_sentences), vocab_size=vocab_size)
 
         return tokenizer_pt, tokenizer_en
+
+    @staticmethod
+    def _batch_iterator(sentences, batch_size=1000):
+        """Yields successive batches of sentences.
+
+        Args:
+            sentences: list of str sentences.
+            batch_size: number of sentences per yielded batch.
+
+        Yields:
+            list of str, a batch of sentences.
+        """
+        for i in range(0, len(sentences), batch_size):
+            yield sentences[i:i + batch_size]
