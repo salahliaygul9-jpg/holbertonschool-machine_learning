3bef6b8b7e00baf0775d9a5142152788114511d9
salahliaygul9-jpg <salahliaygul9-jpg>
2026-07-25 22:13:16 +0000

    dataset

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,65 +1,74 @@
 #!/usr/bin/env python3
-"""Defines the Dataset class used to load and prep a machine
-translation dataset for Portuguese to English translation.
 """
+Dataset module.
+"""
+import transformers
 from setup import load_pt2en
-from transformers import AutoTokenizer
 
 
 class Dataset:
-    """Loads and preps a dataset for machine translation."""
+    """
+    Loads and prepares a dataset for machine translation.
+    """
 
     def __init__(self):
-        """Class constructor.
-
-        Sets the following public instance attributes:
-            data_train: the ted_hrlr_translate/pt_to_en train split,
-                loaded via load_pt2en('train')
-            data_valid: the ted_hrlr_translate/pt_to_en validation
-                split, loaded via load_pt2en('validation')
-            tokenizer_pt: the Portuguese tokenizer created from the
-                training set
-            tokenizer_en: the English tokenizer created from the
-                training set
+        """
+        Initializes the Dataset instance.
+        Loads the train and validation datasets and creates the tokenizers.
         """
         self.data_train = load_pt2en('train')
         self.data_valid = load_pt2en('validation')
+
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+            self.data_train
+        )
 
     def tokenize_dataset(self, data):
-        """Creates sub-word tokenizers for the dataset.
+        """
+        Creates sub-word tokenizers for the dataset.
 
         Args:
-            data: a tf.data.Dataset whose examples are formatted as
-                a tuple (pt, en):
-                    pt is the tf.Tensor containing the Portuguese
-                        sentence
-                    en is the tf.Tensor containing the corresponding
-                        English sentence
+            data (tf.data.Dataset): Dataset containing tuples of
+                (pt, en) formatted as tf.Tensor strings.
 
         Returns:
-            tokenizer_pt, tokenizer_en
-                tokenizer_pt is the Portuguese tokenizer
-                tokenizer_en is the English tokenizer
+            tuple: (tokenizer_pt, tokenizer_en) containing the
+                trained Portuguese and English tokenizers.
         """
-        pt_base = AutoTokenizer.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased')
-        en_base = AutoTokenizer.from_pretrained('bert-base-uncased')
+        pt_model = 'neuralmind/bert-base-portuguese-cased'
+        en_model = 'bert-base-uncased'
 
-        def pt_sentences():
-            """Yields decoded Portuguese sentences from data."""
-            for pt, _ in data.as_numpy_iterator():
-                yield pt.decode('utf-8')
+        # Load the base pre-trained tokenizers
+        tokenizer_pt_base = transformers.AutoTokenizer.from_pretrained(
+            pt_model
+        )
+        tokenizer_en_base = transformers.AutoTokenizer.from_pretrained(
+            en_model
+        )
 
-        def en_sentences():
-            """Yields decoded English sentences from data."""
-            for _, en in data.as_numpy_iterator():
-                yield en.decode('utf-8')
+        def pt_iterator():
+            """
+            Generator yielding batches of Portuguese strings.
+            Using batching speeds up the tokenizer training process.
+            """
+            for pt, _ in data.batch(1000).as_numpy_iterator():
+                yield [sentence.decode('utf-8') for sentence in pt]
 
-        tokenizer_pt = pt_base.train_new_from_iterator(
-            pt_sentences(), vocab_size=2 ** 13)
-        tokenizer_en = en_base.train_new_from_iterator(
-            en_sentences(), vocab_size=2 ** 13)
+        def en_iterator():
+            """
+            Generator yielding batches of English strings.
+            Using batching speeds up the tokenizer training process.
+            """
+            for _, en in data.batch(1000).as_numpy_iterator():
+                yield [sentence.decode('utf-8') for sentence in en]
+
+        # Train new tokenizers based on the dataset iterators
+        vocab_size = 2 ** 13
+        tokenizer_pt = tokenizer_pt_base.train_new_from_iterator(
+            pt_iterator(), vocab_size
+        )
+        tokenizer_en = tokenizer_en_base.train_new_from_iterator(
+            en_iterator(), vocab_size
+        )
 
         return tokenizer_pt, tokenizer_en
